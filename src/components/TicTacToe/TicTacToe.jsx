1d7cfb9ff6414c6a0f84f06de31bf5ddb6924226
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-04-11 00:20:46 -0400

    Fixed win conditions not being saved in the editor

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -54,7 +54,8 @@ const TicTacToe = () => {
       won(data[8]);
     }
     else if(data[0] === data[3] && data[3] === data[6] && data[6] !== '') {
-  }
+      won(data[6]);
+    }
     else if(data[1] === data[4] && data[4] === data[7] && data[7] !== '') {
       won(data[7]);
     }
@@ -62,17 +63,12 @@ const TicTacToe = () => {
       won(data[8]);
     }
     else if(data[0] === data[4] && data[4] === data[8] && data[8] !== '') {
-      won(data[7]);
-    }
-    else if(data[0] === data[1] && data[1] === data[2] && data[2] !== '') {
       won(data[8]);
     }
     else if(data[2] === data[4] && data[4] === data[6] && data[6] !== '') {
       won(data[6]);
     }
-
-
-}
+  }
 
 
 
@@ -104,7 +100,7 @@ const TicTacToe = () => {
         </div> 
 
       </div>
-      
+
       <button className='reset' onClick={() => {window.location.reload()}}>Reset</button>
     </div>
   );
